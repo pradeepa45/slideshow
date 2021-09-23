b391a3ab6fd74e71e7da5b051ae5036b185184d2
pradeepa45 <pradeepa.seelam@gmail.com>
2021-09-23 18:23:31 +0530

    submission

--- src/components/Slides.js
+++ src/components/Slides.js
@@ -1,21 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Slides({slides}) {
-
+    const len = slides.length;
+    const initialValue = 0;
+    const isPrevDisabled = () => {
+        if (slideNumber === 0) return true;
+        return false;
+    }
+    const isNextDisabled = () => {
+        if (slideNumber === len - 1) return true;
+        return false;
+    }
+    const prevSlide = () =>{
+        if(isPrevDisabled()){
+            return null;
+        }
+        setSlide(slideNumber-1);
+    }
+    const nextSlide = () => {
+        if(isNextDisabled()){
+            return null;
+        }
+        setSlide(slideNumber+1);
+    }
+    const restart = () => {
+        setSlide(initialValue);
+    }
+    const [slideNumber, setSlide] = useState(initialValue);
+    console.log(slideNumber);
     return (
-        <div>
-            <div id="navigation" className="text-center">
-                <button data-testid="button-restart" className="small outlined">Restart</button>
-                <button data-testid="button-prev" className="small">Prev</button>
-                <button data-testid="button-next" className="small">Next</button>
+        <React.Fragment>
+            <div>
+                <div id="navigation" className="text-center">
+                    <button data-testid="button-restart" className="small outlined" disabled={isPrevDisabled()} onClick={restart}>Restart</button>
+                    <button data-testid="button-prev" className="small" disabled={isPrevDisabled()} onClick={prevSlide}>Prev</button>
+                    <button data-testid="button-next" className="small" disabled={isNextDisabled()} onClick={nextSlide}>Next</button>
+                </div>
+                <div id="slide" className="card text-center">
+                <h1 data-testid="title">{slides[slideNumber].title}</h1>
+                <p data-testid="text">{slides[slideNumber].text}</p>
             </div>
-            <div id="slide" className="card text-center">
-                <h1 data-testid="title">Slide Title Here</h1>
-                <p data-testid="text">Slide Text Here</p>
             </div>
-        </div>
+        </React.Fragment>
     );
-
 }
-
 export default Slides;
